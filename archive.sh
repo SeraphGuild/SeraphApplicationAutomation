389457b4ddc0c7fbac710ac3e2670f2f34c85ce8
Michael Cook <mcook4728@gmail.com>
2021-11-26 00:08:04 -0600

    tweaks to zip script

--- archive.sh
+++ archive.sh
@@ -1,9 +1,9 @@
+#!/bin/bash
 tarball=$(npm list --depth 0 | sed 's/@/-/g; s/ .*/.tgz/g; 1q;')
 mkdir package
 tar -xzf $tarball
 cp -r dist/* package
-pushd .
-cd package
+pushd package
 zip -rq ../package.zip ./*
 popd
 rm -rf dist package $tarball
