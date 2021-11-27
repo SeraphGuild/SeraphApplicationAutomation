ff353b6a57afe23881b084bbb64493663280d8c2
Michael Cook <mcook4728@gmail.com>
2021-11-27 08:56:10 -0600

    I think I have debugging working now

--- archive.sh
+++ archive.sh
@@ -3,7 +3,7 @@ tarball=$(npm list --depth 0 | sed 's/@/-/g; s/ .*/.tgz/g; 1q;')
 mkdir package
 tar -xzf $tarball
 cp -r dist/* package
-# pushd package
-# zip -rq ../package.zip ./*
-# popd
-# rm -rf dist package $tarball
+pushd package
+zip -rq ../package.zip ./*
+popd
+rm -rf dist package $tarball
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import host from './host.json';
+import settings from './local.settings.json';
