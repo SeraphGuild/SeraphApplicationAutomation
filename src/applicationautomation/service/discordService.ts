a7fe973ee91b20bc8bd8203da2edf371034d70d2
Michael Cook <mcook4728@gmail.com>
2023-04-08 20:09:56 -0700

    forgot a comma

--- src/applicationautomation/service/discordService.ts
+++ src/applicationautomation/service/discordService.ts
@@ -39,7 +39,7 @@ const ClassColorCodeMap: StringKeyMap<number> = {
 const TeamRoleIds: StringKeyMap<string> = {
     '1. Barely Heroic': '1065664768003805194',
     '2. Casually Dysfunctional': '352083485420290058',
-    '3. Misfits': '438160508714221578'
+    '3. Misfits': '438160508714221578',
     '4. Reunion': '1094404428347474081'
 };
 
